a5502bc39d21f595cea9bb78a0042f1c6655a96c
tanch0 <tanchohanglimbu2@gmail.com>
2024-10-07 21:23:46 +0545

    (start):FirstCommit

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+
+export default function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+      
+    </div>
+  )
+}
